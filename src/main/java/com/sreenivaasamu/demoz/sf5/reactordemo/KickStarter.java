f91f51a7213f9d1909284b1e0d3d34060454fa32
Sreenu Gopala <sreenugopala007@outlook.com>
2018-02-13 12:17:32 +0530

    Baseline commit

---
+++ src/main/java/com/sreenivaasamu/demoz/sf5/reactordemo/KickStarter.java
@@ -0,0 +1,13 @@
+package com.sreenivaasamu.demoz.sf5.reactordemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KickStarter 
+{
+    public static void main( String[] args )
+    {
+        SpringApplication.run(KickStarter.class, args);
+    }
+}
